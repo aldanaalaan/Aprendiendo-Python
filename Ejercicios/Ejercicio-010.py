72ace683362e81a48b62b65b6af202a379deb174
Alan Aldana <alanrodrigoyaelaldana@gmail.com>
2021-07-08 20:17:47 -0500

    Añadido el Ejercicio-010

---
+++ Ejercicios/Ejercicio-010.py
@@ -0,0 +1,13 @@
+# Ciclo While
+
+num1 = int(input('Dime hasta que numero quieres la sucecion de 5 en 5: '))
+
+# Variables
+i = 0
+cad1 = ''
+
+while i <= num1:
+	cad1 += str(i) + ', '
+	i += 5
+
+print(cad1)
