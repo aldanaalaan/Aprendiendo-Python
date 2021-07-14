c94262dd57ee5cdf7be4410a303f57525805fff2
Alan Aldana <alanrodrigoyaelaldana@gmail.com>
2021-07-14 18:06:42 -0500

    Corregido un error de valores en el ultiimo programa subido

--- Test/ArchivoLista/main.py
+++ Test/ArchivoLista/main.py
@@ -10,64 +10,64 @@ while True:
 
     print('Menu de opciones'.upper())
     print('[1] Agregar elemento\n[2] Eliminar elemento\n[3] Mostrar elementos\n[4] Guardar y salir')
-    opc = input('¿Que quieres hacer? ')
-    opc = int(opc)
-    if opc == 1:
+    try:
+        opc = int(input('¿Que quieres hacer? '))
+    except ValueError:
+        print('Debes ingresar un numero entero')
+        continue
+    else:
+    
+        if opc == 1:
+            try:
+                elemento = int(input('Dime que elemento agregar: '))
+            except ValueError:
+                print('Debes ingresar un valor enteros.')
+                continue
+            else:
+                l.append(elemento)
 
-        elemento = input('Dime que elemento agregar: ')
-        elemento = int(elemento)
-        l.append(elemento)
+        elif opc == 2:
+            if l == []:
+                print('La lista ya está vacia.')
+                continue
 
-    elif opc == 2:
+            contador = 1
 
-        if l == []:
+            for i in l:
+                print(str(contador) + '. ' + str(i))
+                contador += 1
 
-            print('La lista ya está vacia.')
-            continue
+            try:
+                numEl = int(input('Dime que elemento eliminar: '))
+            except ValueError:
+                print('Debes ingresar un numero entero.')
+                continue
+            if numEl > len(l) or numEl < 1:
+                print('El elemento seleccionado no se encuentra en la lista.')
+                continue
 
-        contador = 1
+            l.remove(l[numEl - 1])
+            print(f'Elemento {numEl} eliminado.')
 
-        for i in l:
+        elif opc == 3:
+            if l == []:
+                print('La lista está vacia.')
+                continue
 
-            print(str(contador) + '. ' + str(i))
-            contador += 1
+            contador = 1
+            for i in l:
+                print(str(contador) + '. ' + str(i))
+                contador += 1
 
-        numEl = input('Dime que elemento eliminar: ')
-        numEl = int(numEl)
+        elif opc == 4:
+            print('Adios usuario')
+            break
 
-        if numEl > len(l) or numEl < 1:
+        else :
+    	    print('La opcion no está disponible.')
+    handled.close()
+    handled = open('lista.txt', 'w')
 
-            print('El elemento seleccionado no se encuentra en la lista.')
-            continue
-
-        l.remove(l[numEl - 1])
-        print(f'Elemento {numEl} eliminado.')
-
-    elif opc == 3:
-
-        if l == []:
-            print('La lista está vacia.')
-            continue
-
-        contador = 1
-        for i in l:
-
-            print(str(contador) + '. ' + str(i))
-            contador += 1
-
-    elif opc == 4:
-
-        print('Adios usuario')
-        break
-
-    else :
-
-    	print('La opcion no está disponible.')
-handled.close()
-handled = open('lista.txt', 'w')
-
-for i in l:
-
-    handled.write(str(i) + '\n')
-
-handled.close()
+    for i in l:
+        handled.write(str(i) + '\n')
+    handled.close()
