5b6fb7f557bef55a329422a84f5e9325891f5b3d
Alan Aldana <alanrodrigoyaelaldana@gmail.com>
2021-07-16 09:11:16 -0500

    Agregados los ejercicios 016 y 017 a Ejercicios

---
+++ Ejercicios/Ejercicio-016.py
@@ -0,0 +1,16 @@
+# Diccionarios
+edades = {'Cristina':17,'David':6,'Alan':17,'Fani':9}
+
+# Impresion del diccionario
+print(edades)
+
+#Impresion de un dato especifico
+print(edades['David'])
+
+edades['Fani'] = 10
+
+# X2
+print(edades)
+
+for i in edades:
+    print(i,edades[i])
---
+++ Ejercicios/Ejercicio-017.py
@@ -0,0 +1,6 @@
+# Funciones del diccionario
+
+notas = {'Alan':75,'Fani':96,'David':70}
+
+for i in notas:
+    print(i,notas[i])
